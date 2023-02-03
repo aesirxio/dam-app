9b7d7366a5cd668cda3e4e96372f1eafe2297edf
huy <huytran.dev.web@gmail.com>
2023-02-03 18:08:40 +0700

    hot fix i18n conflict

--- src/components/SwitchThemes/index.jsx
+++ src/components/SwitchThemes/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import './index.scss';
 
 import { withThemeContext } from 'themes/ThemeContextProvider';
 import ThemeMode from 'SVG/ThemeMode';
-
 class SwitchThemes extends React.Component {
   render() {
     const { theme, changeTheme } = this.props;
     return (
-      <div className=" position-relative cursor-pointer">
+      <div className={`position-relative cursor-pointer`}>
         <ThemeMode fill={theme.color} onClick={() => changeTheme(theme)} />
       </div>
     );
