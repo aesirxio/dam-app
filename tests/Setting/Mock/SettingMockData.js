a07850a1fe5c5b3777342c953aa776a14dde4dd6
trang <trang@r-digital.tech>
2023-02-07 13:13:31 +0700

    Remove storeAWS

--- tests/Setting/Mock/SettingMockData.js
+++ tests/Setting/Mock/SettingMockData.js
@@ -3,11 +3,7 @@ class SettingMockData
     static getInfoToUpdateStoreAWS()
     {
         const storeAWS = {
-            "name": "AWS",
-            "key":"AKIAUTQPXIYMPBPDLK7M",
-            "secret":"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",
-            "region":"ap-east-1",
-            "bucket":"qa.aesirx.io"
+           
         };
         return storeAWS;
     }
