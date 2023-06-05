e3d66837433a1aa302ceeb99a4d05d749dd719d6
Lu Nguyen <lu@r-digital.tech>
2023-06-05 20:05:59 +0700

    Optimize tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,23 +6,28 @@ import inlineImage from 'esbuild-plugin-inline-image';
 const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
-  clean: true,
+  clean: env !== 'development',
   format: ['esm'],
-  minify: env === 'production',
+  minify: env === 'development' ? false : 'terser',
+  watch: env === 'development',
   outDir: 'dist',
-  entry: ['src/integration/index.js'],
-  outExtension() {
-    return {
-      js: `.js`,
-    };
-  },
+  entry: ['src/integration/index.js', 'src/integration/**/*.{js,jsx}'], //entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
+  target: 'es2020',
+  platform: 'browser',
   esbuildPlugins: [
     inlineImage({ limit: -1 }),
     ScssModulesPlugin({ localsConvention: 'dashes' }),
     sassPlugin({ type: 'style' }),
   ],
+  outExtension() {
+    return {
+      js: `.js`,
+    };
+  },
+  terserOptions: {
+    compress: { drop_console: true },
+  },
   loader: {
     '.js': 'jsx',
   },
-  target: 'es2020',
 };
