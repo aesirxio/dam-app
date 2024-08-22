e5ca62ef87f6e967f883c5b7dcb9b94bcd16ebca
Gia Phu <phu.tran@r-digital.tech>
2024-08-22 17:51:32 +0700

    fix calculate byte

--- packages/aesirx-dam-app/src/components/Storage/index.jsx
+++ packages/aesirx-dam-app/src/components/Storage/index.jsx
@@ -20,7 +20,7 @@ const mbToByte = 1000000;
 function formatBytes(bytes, decimals = 2) {
   if (!+bytes) return 0;
 
-  const k = 1024;
+  const k = 1000;
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
